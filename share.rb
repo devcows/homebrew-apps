4d0148ed6d64b1262b4d2f02078bd599b97b77ff
Guillermo Guerrero <wolf.fox1985@gmail.com>
2016-06-23 00:44:58 +0200

    Update to v0.0.2

--- share.rb
+++ share.rb
@@ -1,11 +1,32 @@
 class Share < Formula
-  desc ""
-  homepage ""
-  url "https://github.com/devcows/share/releases/download/v0.0.1/share_0.0.1_darwin_amd64.zip"
-  version "64"
-  sha256 "e0ac107a5023cd7125cfa88464a856616092bdc8ab26143173b3e801ce953c95"
+  desc "Share is a command line for sharing files by network. The goal is to share a file easy as human-friendly as possible. It provides an independent webserver for each file."
+  homepage "https://github.com/devcows/share"
+  version "0.0.2"
+
+  url "https://github.com/devcows/share/releases/download/v0.0.2/share_0.0.2_darwin_amd64.zip"
+  sha1 "edf07c9a9152155cfffa1526acfd13a78df186be"
+
+  if OS.linux?
+    if Hardware.is_64_bit?
+      url "https://github.com/devcows/share/releases/download/v0.0.2/share_0.0.2_linux_amd64.tar.gz"
+      sha1 "a16f006aaf349278d37b2c29c697b2d9a7b06f74"
+    end
+  end
+
+  depends_on :arch => :intel
 
   def install
     bin.install "share"
+  end
+
+  def caveats
+    msg = <<-"EOF"
+    ____  _
+   / ___|| |__   __ _ _ __ ___
+   \\___ \\| '_ \\ / _` | '__/ _ \\
+    ___) | | | | (_| | | |  __/
+   |____/|_| |_|\\__,_|_|  \\___|
+
+EOF
   end
 end
