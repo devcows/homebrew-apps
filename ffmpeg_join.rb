e96d2652b51c4d7504a863e91732caae546fff12
Guillermo Guerrero Ibarra <guillermo.guerrero@deliveroo.co.uk>
2021-09-03 14:45:51 +0200

    Fixed url again.

--- ffmpeg_join.rb
+++ ffmpeg_join.rb
@@ -7,14 +7,14 @@ class FfmpegJoin < Formula
 
   if OS.linux?
     if Hardware::CPU.is_64_bit?
-      url "#{BASE_URL}/#{VERSION_NUM}/ffmpeg_join_#{VERSION_NUM}_linux_amd64.tar.gz"
+      url "#{BASE_URL}/#{VERSION_NUM}/ffmpeg_join-#{VERSION_NUM}-linux-amd64.tar.gz"
       sha256 "5693d7837e3f72ccff1acf2e2ea6f2342e50f36a07ffb10e85ac7e2d6e5507a2"
 		else
-      url "#{BASE_URL}/#{VERSION_NUM}/ffmpeg_join_#{VERSION_NUM}_linux_386.tar.gz"
+      url "#{BASE_URL}/#{VERSION_NUM}/ffmpeg_join-#{VERSION_NUM}-linux-386.tar.gz"
       sha256 "659ed4af693a38a2c582415b4f3989a950a6a1122adca553327065c8032acf77"
 		end
   else
-    url "#{BASE_URL}/#{VERSION_NUM}/ffmpeg_join_#{VERSION_NUM}_darwin_amd64.tar.gz"
+    url "#{BASE_URL}/#{VERSION_NUM}/ffmpeg_join-#{VERSION_NUM}-darwin-amd64.tar.gz"
     sha256 "fa03cb28f9821fe35f9a512ed9f7ca9c05b9630a5e91591b5c177f23addeff9f"
   end
 
